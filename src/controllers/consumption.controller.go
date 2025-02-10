11a9cac52751e21e70c97031c93045ea1dd52db1
JuanConde27 <condejuan27@gmail.com>
2025-02-10 09:54:59 -0500

    Mejora de codigo en controller y validators

--- src/controllers/consumption.controller.go
+++ src/controllers/consumption.controller.go
@@ -1,12 +1,10 @@
 package controllers
 
 import (
-	"encoding/json"
 	"net/http"
-	"strconv"
-	"strings"
-	"time"
-
+	
+	"github.com/JuanConde27/energy-microservice/src/config"
+	"github.com/JuanConde27/energy-microservice/src/utils"
 	"github.com/JuanConde27/energy-microservice/src/services"
 )
 
@@ -18,67 +16,20 @@ func NewConsumptionController(service *services.ConsumptionService) *Consumption
 	return &ConsumptionController{Service: service}
 }
 
-func parseMeterIDs(param string) ([]int, error) {
-	ids := strings.Split(param, ",")
-	var result []int
-	for _, id := range ids {
-		parsedID, err := strconv.Atoi(id)
-		if err != nil {
-			return nil, err
-		}
-		result = append(result, parsedID)
-	}
-	return result, nil
-}
-
-func isValidPeriod(kindPeriod string) bool {
-	validPeriods := map[string]bool{"daily": true, "weekly": true, "monthly": true}
-	return validPeriods[kindPeriod]
-}
-
-func isValidDate(dateStr string) (time.Time, error) {
-	layout := "2006-01-02"
-	return time.Parse(layout, dateStr)
-}
-
 func (cc *ConsumptionController) GetConsumption(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query()
-	
-	meterIDs, err := parseMeterIDs(query.Get("meters_ids"))
-	if err != nil {
-		http.Error(w, "❌ Invalid meter_ids format. Expected comma-separated integers.", http.StatusBadRequest)
-		return
-	}
+    query := r.URL.Query()
 
-	startDate, err := isValidDate(query.Get("start_date"))
-	if err != nil {
-		http.Error(w, "❌ Invalid start_date format. Expected YYYY-MM-DD.", http.StatusBadRequest)
-		return
-	}
+    meterIDs, startDate, endDate, kindPeriod, statusCode, errorMessage := utils.ValidateQueryParams(query)
+    if statusCode != http.StatusOK {
+        http.Error(w, errorMessage, statusCode)
+        return
+    }
 
-	endDate, err := isValidDate(query.Get("end_date"))
-	if err != nil {
-		http.Error(w, "❌ Invalid end_date format. Expected YYYY-MM-DD.", http.StatusBadRequest)
-		return
-	}
+    response, err := cc.Service.GetConsumption(meterIDs, startDate.Format("2006-01-02"), endDate.Format("2006-01-02"), kindPeriod)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 
-	if startDate.After(endDate) {
-		http.Error(w, "❌ start_date cannot be later than end_date.", http.StatusBadRequest)
-		return
-	}
-
-	kindPeriod := query.Get("kind_period")
-	if !isValidPeriod(kindPeriod) {
-		http.Error(w, "❌ Invalid kind_period. Only 'daily', 'weekly', or 'monthly' are allowed.", http.StatusBadRequest)
-		return
-	}
-
-	response, err := cc.Service.GetConsumption(meterIDs, startDate.Format("2006-01-02"), endDate.Format("2006-01-02"), kindPeriod)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+    config.SendResponse(w, http.StatusOK, response)
 }
